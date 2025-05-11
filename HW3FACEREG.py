5030b5bc12702f89b41231f078d253db95612424
NguyenDaoTrongTin <tin.nguyen2402061@std.ttu.edu.vn>
2025-05-11 15:39:18 +0700

    Update HW3FACEREG.py
    fix to change the size of elements of window

--- HW3FACEREG.py
+++ HW3FACEREG.py
@@ -1,16 +1,20 @@
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import font as tkfont
 # Create the main window
 root = tk.Tk()
 root.title("Left and Right Frames")
 root.geometry("800x500")
 
 # Maximize the window
-root.state('zoomed')
+#root.state('zoomed')
+
+#create a dynamic font
+dynamic_font = tkfont.Font(family='Arial', size=20)
 
 # Create a frame inside the window
-frame1 = tk.Frame(root, bg="white", padx=10, pady=10)
-frame1.pack(fill='both', expand=True)
+frame1 = tk.Frame(root, bg="white")
+frame1.place(relx=0, rely=0, relwidth=0.25, relheight=1)
 
 def upload_file():
     file_path = filedialog.askopenfilename(title="Select a File")
@@ -21,17 +25,23 @@ def upload_file():
 
 
 # Create buttons and add them to the frame
-button1 = tk.Button(frame1, text="WEBCAM", width=10, height=1, bd =5, font=('Arial', 20), command= lambda: print("Button 1 clicked"))
-button1.pack(side='top', anchor='w', padx=10, pady=10)
+button1 = tk.Button(frame1, text="WEBCAM", font=dynamic_font, command=lambda: print("Button 1 clicked"))
+button1.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.1)
 
-button2 = tk.Button(frame1, text="VIDEO", width=10, height=1, bd =5, font=('Arial', 20), command= upload_file)#lambda: print("Button 2 clicked"))
-button2.pack(side='top', anchor='w', padx=10, pady=10)
+button2 = tk.Button(frame1, text="VIDEO", font=dynamic_font, command=upload_file)
+button2.place(relx=0.1, rely=0.25, relwidth=0.8, relheight=0.1)
 
+frame2 = tk.Frame(root, bg="lightblue")
+frame2.place(relx=0.25, rely=0, relwidth=0.75, relheight=1)
 
-frame2 = tk.Frame(root, bg="lightblue", width=1070, height=640)
-frame2.place(x=200, y=5)
+# rezize font of buttons
+def resize_font(event):
+    button_height = button1.winfo_height()
+    new_size = max(7, int(button_height * 0.4))
+    dynamic_font.configure(size=new_size)
 
+# call resize_font when changing window
+root.bind("<Configure>", resize_font)
 
 # Run the application
 root.mainloop()
-
