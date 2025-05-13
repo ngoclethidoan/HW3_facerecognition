a332bc8f00e39ae8b4badaf30cf5877081d5ff42
NguyenDaoTrongTin <tin.nguyen2402061@std.ttu.edu.vn>
2025-05-13 14:25:57 +0700

    Remake
    changing interface formed by menu(include: Open File, Web cam, Exist).

--- HW3FACEREG.py
+++ HW3FACEREG.py
@@ -1,47 +1,27 @@
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, mainloop
+from tkinter import messagebox
 from tkinter import font as tkfont
-# Create the main window
+
 root = tk.Tk()
-root.title("Face recognition")
+root.title('Menu Demonstration')
 root.geometry("800x500")
 
-# Maximize the window
-#root.state('zoomed')
+# Creating Menubar
+menubar = tk.Menu(root)
 
-#create a dynamic font
-dynamic_font = tkfont.Font(family='Arial', size=20)
-
-# Create a frame inside the window
-frame1 = tk.Frame(root, bg="white")
-frame1.place(relx=0, rely=0, relwidth=0.25, relheight=1)
-
-def upload_file():
+def video():
     file_path = filedialog.askopenfilename(title="Select a File")
-    print("Selected file:", file_path)
-    if file_path:  # If a file is selected
-        # Change the color of the button after the file is uploaded
-        button2.config(bg="lightblue")#, text="Uploaded!")
+    print(file_path)
 
+# Adding File Menu and commands
+menu = tk.Menu(menubar, tearoff=0)
+menubar.add_cascade(label='Menu', menu=menu)
+menu.add_command(label='New File', command=video)
+menu.add_command(label='WebCam', command=None)
+menu.add_separator()
+menu.add_command(label='Exit', command=root.destroy)
 
-# Create buttons and add them to the frame
-button1 = tk.Button(frame1, text="WEBCAM", font=dynamic_font, command=lambda: print("Button 1 clicked"))
-button1.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.1)
-
-button2 = tk.Button(frame1, text="VIDEO", font=dynamic_font, command=upload_file)
-button2.place(relx=0.1, rely=0.25, relwidth=0.8, relheight=0.1)
-
-frame2 = tk.Frame(root, bg="lightblue")
-frame2.place(relx=0.25, rely=0, relwidth=0.75, relheight=1)
-
-# rezize font of buttons
-def resize_font(event):
-    button_height = button1.winfo_height()
-    new_size = max(7, int(button_height * 0.4))
-    dynamic_font.configure(size=new_size)
-
-# call resize_font when changing window
-root.bind("<Configure>", resize_font)
-
-# Run the application
-root.mainloop()
+# display Menu
+root.config(menu=menubar)
+mainloop()
